3544ce51ee4e3aa05bd834221268ae6cc8757b2d
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-05 13:32:25 +0700

    Days In Month

---
+++ src/dayOfMonth.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class dayOfMonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which month that you want to count days? ");
+        int month = scanner.nextInt();
+        String daysInMonth = switch (month) {
+            case 2 -> "28 or 29";
+            case 1, 3, 5, 7, 8, 10, 12 -> "31";
+            case 4, 6, 9, 11 -> "30";
+            default -> "";
+        };
+        if( !daysInMonth.equals("")) {
+            System.out.printf("The month '%d' has %s days!", month, daysInMonth);
+        } else {
+            System.out.println("Invalid input!");
+        }
+    }
+}
